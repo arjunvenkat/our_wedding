767629927990bc46aadd04c98ae28a99213ce891
Arjun Venkataswamy <avenkat2@gmail.com>
2016-04-08 10:33:12 -0500

    adds super admin functionality to prevent accidental deletion of data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402191227) do
+ActiveRecord::Schema.define(version: 20160408152530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20160402191227) do
     t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.boolean  "super_admin",            default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
